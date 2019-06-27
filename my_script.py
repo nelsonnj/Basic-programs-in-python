535fa333cd59fa4e67091c123b8a5d75fabbb867
Nelson <nelsonjayawanth@gmail.com>
2019-06-27 13:39:02 +0530

    created new file for creating array in numpy

---
+++ my_script.py
@@ -0,0 +1,25 @@
+''' Array using numpy module '''
+import numpy as np
+x=np.array([1,2,3.4,8,12])
+
+# Printing array
+
+print(x)
+
+# Finding mean value from array
+mean_val=np.mean(x)
+
+print(mean_val)
+
+
+# Finding max value from array
+
+max_val=np.max(x)
+
+print(max_val)
+
+# Finding min value from array
+
+min_val=np.min(x)
+
+print(min_val)
